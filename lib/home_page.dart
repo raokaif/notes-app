5ebc54e8a74114b53ddade9c7c77b5be8f93c865
kaif <kaifrao250@gmail.com>
2025-07-03 23:08:56 +0500

    home page

---
+++ lib/home_page.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(249, 249, 249, 1),
+      appBar: AppBar(
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text('Edit', style: Theme.of(context).textTheme.titleSmall),
+          ),
+        ],
+        backgroundColor: const Color.fromRGBO(249, 249, 249, 1),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Folders', style: Theme.of(context).textTheme.titleLarge),
+            SizedBox(height: 10),
+            SizedBox(
+              height: 35,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.grey[200],
+
+                  hintText: 'Search',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 18),
+            GestureDetector(
+              onTap: () {},
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                tileColor: Colors.white,
+
+                leading: Icon(
+                  Icons.note_outlined,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: Text('Quick Notes'),
+                trailing: SizedBox(
+                  width: 40,
+                  child: Row(
+                    children: [
+                      Text('1'),
+                      SizedBox(width: 8),
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: ListTile(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                tileColor: Colors.white,
+
+                leading: Icon(
+                  Icons.folder_outlined,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: Text('Notes'),
+                trailing: SizedBox(
+                  width: 40,
+                  child: Row(
+                    children: [
+                      Text('1'),
+                      SizedBox(width: 8),
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.create_new_folder_outlined),
+            label: '',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.folder), label: ''),
+        ],
+        currentIndex: 0,
+        onTap: (index) {},
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,10 +12,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
+        primaryColor: Colors.yellow,
+        textTheme: TextTheme(
+          titleLarge: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          titleSmall: TextStyle(fontSize: 18, color: Colors.yellow),
+        ),
       ),
-      home: Placeholder(),
+      home: HomePage(),
     );
   }
 }
