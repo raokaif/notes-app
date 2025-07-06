83c87524ac1c730b28ed964b4a51829957913f07
kaif <kaifrao250@gmail.com>
2025-07-06 18:20:32 +0500

    textfield

--- lib/add_note_page.dart
+++ lib/add_note_page.dart
@@ -9,13 +9,38 @@ class AddNotePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.share)),
           IconButton(
             onPressed: () {},
-            icon: Icon(CupertinoIcons.ellipsis_circle),
+            icon: Icon(CupertinoIcons.share, color: Colors.amber, size: 25),
           ),
-          TextButton(onPressed: () {}, child: Text('Done')),
+          SizedBox(width: 20),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              CupertinoIcons.ellipsis_circle,
+              color: Colors.amber,
+              size: 25,
+            ),
+          ),
+          SizedBox(width: 20),
+
+          TextButton(
+            onPressed: () {},
+            child: Text('Done', style: Theme.of(context).textTheme.titleSmall),
+          ),
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          cursorHeight: 50,
+          cursorColor: Colors.black,
+          expands: true,
+          maxLines: null,
+          minLines: null,
+          keyboardType: TextInputType.multiline,
+          decoration: InputDecoration(border: InputBorder.none),
+        ),
       ),
     );
   }
